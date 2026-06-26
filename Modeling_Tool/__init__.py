3abc237461cc4523f8c09d224238547053326e1c
Kyle <68816744+Kyle-J-Sun@users.noreply.github.com>
2026-06-26 23:50:56 +0800

    chore(release): bump version to 0.1.3
    - pyproject.toml: 0.1.2 -> 0.1.3
    - setup.py: SMF_VERSION default 0.1.2 -> 0.1.3
    - Modeling_Tool/__init__.py: __version__ 0.1.2 -> 0.1.3

--- Modeling_Tool/__init__.py
+++ Modeling_Tool/__init__.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 __author__ = "Jingkai Sun"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 # Core module - base data structures
 # ODPSRunner is intentionally omitted from this eager import so that
