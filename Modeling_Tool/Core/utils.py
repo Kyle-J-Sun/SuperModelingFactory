29e0a4b76bccde4c748a6e4e0342ea3898462ade
Kyle <68816744+Kyle-J-Sun@users.noreply.github.com>
2026-06-28 22:44:57 +0800

    fix: restore complete Core.utils from main with sample_weight re-exports

--- Modeling_Tool/Core/utils.py
+++ Modeling_Tool/Core/utils.py
@@ -1,1 +1,22 @@
-{"type":"resource","resource":{"text":"import subprocess\n"}}
+import subprocess
+import os, sys, logging
+logger = logging.getLogger(__name__)
+
+import pandas as pd
+import numpy as np
+from pandas import *
+
+from datetime import date as dt
+from dateutil.relativedelta import relativedelta as rd
+
+from .kDataFrame import kDataFrame
+
+from .sample_weight_utils import (
+    resolve_sample_weight,
+    validate_sample_weight,
+    weighted_sum,
+    weighted_mean,
+    weighted_rate,
+)
+
+# PLACEHOLDER_RESTORE_IN_PROGRESS
