238c065d44feeae6ddafda1179a8e3d2f5701e51
Kyle <68816744+Kyle-J-Sun@users.noreply.github.com>
2026-06-28 20:57:48 +0800

    fix: stop auto-patching eval at import time

--- Modeling_Tool/Eval/__init__.py
+++ Modeling_Tool/Eval/__init__.py
@@ -5,11 +5,8 @@ from .Model_Eval_Tool import (
     tie_score_rate,
     score_unique_rate,
     get_gains_table_by_cust_metrics,
-#     get_backward_perf,
-#     get_backward_summary,
     GainsTableCalculator,
     PerformanceEvaluator,
-#     BackwardEliminationAnalyzer
 )
 
 from .Evaluation_Tool import (
@@ -42,21 +39,11 @@ from .evaluate_model import (
     resturct_gains,
 )
 
-from Modeling_Tool.weighted_integration import apply_eval_patches
-apply_eval_patches(globals())
-
 __all__ = [
-    # Model_Eval_Tool
     'get_gains_table', 'get_perf_summary', 'cross_risk', 'tie_score_rate',
     'score_unique_rate', 'get_gains_table_by_cust_metrics',
-#     'get_backward_perf', 'get_backward_summary',
     'GainsTableCalculator', 'PerformanceEvaluator',
-#     'BackwardEliminationAnalyzer',
-
-    # Evaluation_Tool
     'EvaluationPipeline', 'Model_Evaluation_Tool', 'Utility_Functions',
-
-    # evaluate_model
     'calc_pr', 'summarize_pr', 'plot_pr_curve',
     'calc_roc', 'plot_ks_curve', 'plot_roc_curve',
     'calc_equid_dist', 'plot_kde_curve', 'plot_dist_curve',
